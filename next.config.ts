232aefc992809d8e148f06ef6ffca2429ce176f8
Kevin Choi <kevinnwjchoi@gmail.com>
2024-12-05 17:57:35 -0500

    added export scripts

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
   reactStrictMode: true,
+  basePath: '/my-portfolio',  // Set the base path to your repo name
+  trailingSlash: true,        // Optionally add trailing slash for clean URLs
 };
 
 export default nextConfig;
