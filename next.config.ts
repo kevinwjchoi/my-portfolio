b8eae316178e16df7002f5390684a65e7f372360
Kevin Choi <kevinnwjchoi@gmail.com>
2024-12-05 18:06:19 -0500

    output:export image fix

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,9 @@ import type { NextConfig } from 'next'
 const nextConfig: NextConfig = {
   reactStrictMode: true,
   output: 'export', // Enable static export
+  images: {
+    unoptimized: true, // Disable image optimization
+  },
 }
 
 export default nextConfig
